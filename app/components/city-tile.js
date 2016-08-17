1fc949fdc7ab9d8a07dbec9882cb6bc58c661313
patrick lipscomb and Wolfgang Warneke <patrick@>
2016-08-17 14:54:03 -0700

    add the many to one relationships

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    destroyCity(city) {
+      if (confirm('Are you sure you want to delete {{city.name}}?')) {
+        console.log(city);
+        this.sendAction('destroyCity', city);
+      }
+    }
+  }
 });
