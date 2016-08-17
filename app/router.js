eb5bb65a78a69807306510e6634639651f222780
patrick lipscomb and Wolfgang Warneke <patrick@>
2016-08-17 10:23:41 -0700

    add mid commit

--- app/router.js
+++ app/router.js
@@ -11,6 +11,7 @@ Router.map(function() {
   this.route('announcement');
   this.route('rental', {path: '/rental/:rental_id'});
   this.route('singleAnnouncement', {path: '/singleAnnouncement/:announcement_id'});
+  this.route('city', {path: '/city/:city_id'});
 });
 
 export default Router;
